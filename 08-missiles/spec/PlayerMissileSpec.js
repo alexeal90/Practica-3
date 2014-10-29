23976963e0fb22524ca1145cb6b0499360082c7d
alexeal90 <alexeal90@gmail.com>
2014-10-29 20:48:25 +0100

    prototipo 08 casi acabado

--- 08-missiles/spec/PlayerMissileSpec.js
+++ 08-missiles/spec/PlayerMissileSpec.js
@@ -43,12 +43,12 @@ describe("PlayerMissile", function(){
        ctx = canvas.getContext('2d');
        expect(ctx).toBeDefined();
        board = new GameBoard();
-       //var oldSpriteSheet = SpriteSheet;
+       oldSpriteSheet = SpriteSheet;
    });
    
-   //afterEach(function(){
-   	//SpriteSheet = oldSpriteSheet;
-   //});
+   afterEach(function(){
+   	SpriteSheet = oldSpriteSheet;
+   });
 
 	it("Prueba PlayerMissile", function() {
 		SpriteSheet.map = {missile: {h:10, w:2}};
@@ -62,7 +62,10 @@ describe("PlayerMissile", function(){
 	});
 
 	it("Draw PlayerMissile", function() {
-		SpriteSheet = {draw: function(){}, map: {missile: {sx: 0, sy: 30, w: 2, h: 10, frames: 1}}};
+		SpriteSheet = {
+			draw: function(){},
+			map: {missile: {sx: 0, sy: 30, w: 2, h: 10, frames: 1}}
+			};
 		var aux = {};
 		disparo = new PlayerMissile(1,1);
 		
@@ -70,7 +73,28 @@ describe("PlayerMissile", function(){
 		disparo.draw(aux);
 		
 		expect(SpriteSheet.draw).toHaveBeenCalled();
-	};
+	});
 
+	it("Step PlayerMissile", function() {
+		disparo = new PlayerMissile(1, 1);
+		var yAux = disparo.y;
+		var cte = -1;
+		//creo un board nuevo con un campo remove vacio, necesario para step.
+		var newBoard = {
+			remove: function(obj) {}
+		};
+		
+		disparo.board = newBoard;
+		spyOn(newBoard, "remove");
+		
+		//while( cte > (-disparo.h-disparo.y)/disparo.vy ) { cte /= 5; };
+		disparo.step(cte);
+		//en estos momentos this.y = 709 será mayor que -this.h = -10 por lo que no se cumplira la condicion para llamara a .remove
+		expect(disparo.y).toBe(yAux + disparo.vy*cte);
+		expect(newBoard.remove).not.toHaveBeenCalled();
+		//modifico el valor del argumento que le paso a .step para que se cumpla la condicion y se llame a .remove con disparo como argumento
+		disparo.step(1+(-disparo.h-disparo.y)/disparo.vy););
+		expect(newBoard.remove).toHaveBeenCalledWith(disparo);
+	});
 
 });
